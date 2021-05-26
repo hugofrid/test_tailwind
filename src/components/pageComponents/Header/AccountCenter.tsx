d00e5338e9dafb1f1e2183fc6e575737ded83498
Hugo Fridlansky <hfrilansky@MacBook-Pro-de-MAC284.local>
2021-05-26 10:30:00 +0200

    first commit

--- src/components/pageComponents/Header/AccountCenter.tsx
+++ src/components/pageComponents/Header/AccountCenter.tsx
@@ -4,10 +4,7 @@ import Avatar from '../../Avatar/Avatar'
 const AccountCenter = () => {
     return (
         <div>
-            <Avatar user={{firstName:"Jean",lastName:"Reno",function:"Adminitstrator"}}/>
-            <Avatar active user={{firstName:"Jean",lastName:"Reno",function:"Adminitstrator"}}/>
-            <Avatar size="lg" user={{firstName:"Jean",lastName:"Reno",function:"Adminitstrator"}}/>
-            <Avatar size="lg" active user={{firstName:"Jean",lastName:"Reno",function:"Adminitstrator"}}/>
+            <Avatar user={{ firstName: "Jean", lastName: "Reno", function: "Adminitstrator" }} />
         </div>
     )
 }
